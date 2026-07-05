ee0a7e567fda7c3ebed6ad7713f73eaed0b06f5a
jordelmir <jordelmir@gmail.com>
2026-07-05 10:35:37 -0600

    chore: bump compileSdk and targetSdk to 35 for Play Store compliance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.supremecorp.bass"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.supremecorp.bass"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
